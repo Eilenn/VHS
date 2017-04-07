1b14aad9f7a01a75a638ef3a88eb39f66dccbcab
Bogna <wrobel.bogna@gmail.com>
2017-04-07 12:16:45 +0200

    with colors for invalid input

---
+++ src/app/mock-customers.ts
@@ -0,0 +1,11 @@
+import { Customer } from "./customer";
+
+let customer=new Customer('John', 'Wick');
+let customer2=new Customer('Wade', 'Wilson');
+
+export const CUSTOMERS: Customer[] = [
+   {firstName:'John',lastName:'Wick'},
+   {firstName:'John',lastName:'Smith'},
+   {firstName:'Wade',lastName:'Wilson'},
+   {firstName:'John',lastName:'Bean'}
+];
--- src/app/subscribed-customers.component.ts
+++ src/app/subscribed-customers.component.ts
@@ -1,9 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Customer } from "./customer";
+import { CUSTOMERS } from "./mock-customers";
 
 @Component({
   selector: 'subscribed-customers',
   templateUrl: './templates/subscribed-customers.component.html'
 })
-export class SubscribedCustomersComponent{
+export class SubscribedCustomersComponent implements OnInit {
+    ngOnInit(): void {
+      this.customers=CUSTOMERS;
+    }
 
+customers: Customer[];
 }
