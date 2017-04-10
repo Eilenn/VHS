daae24c5a7806760022875af0d4324f544139355
Bogna <wrobel.bogna@gmail.com>
2017-04-10 08:53:42 +0200

    with additional fee and sum alert

--- src/app/collect-tape-form-component.ts
+++ src/app/collect-tape-form-component.ts
@@ -11,12 +11,16 @@ import { TAPES } from "./mock-tapes";
 export class CollectTapeFormComponent{
     @Input() tapeToCollect: Tape;
 
-    onSubmit(rating: number) {
+    onSubmit(rating: number, fee: number, additionalFee: number) {
         this.tapeToCollect.status = TapeStatus.AVAILABLE;
         this.tapeToCollect.customer=null;
         this.tapeToCollect.rentDate=null;
         this.tapeToCollect.returnDate=null;
         this.tapeToCollect.rating=rating;
-        alert("Tape has been collected");
+        if(additionalFee===undefined){
+            additionalFee=0;
+        }
+        let sum=fee+additionalFee;
+        alert("Fee equals "+sum);
     }
 }
